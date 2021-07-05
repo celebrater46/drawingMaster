a75e8bba89410bebb6c28ed41bfd97c00f273c76
hideru <jota.16.stigma.world@gmail.com>
2021-07-06 02:49:21 +0900

    created getList()

--- main.php
+++ main.php
@@ -10,14 +10,34 @@ error_reporting(E_ALL);
 //echo('種類: ' . $_REQUEST['mode'] . PHP_EOL);
 //echo('間隔: ' . $_REQUEST['interval'] . PHP_EOL);
 
-$landscapes = glob('./img/landscapes/*');
-$animals = glob('./img/animals/*');
-$vehicles = glob('./img/vehicles/*');
-$allPhotos = array_merge($landscapes, $animals, $vehicles);
+function getList($pic) {
+    $landscapes = glob('./img/landscapes/*');
+    $animals = glob('./img/animals/*');
+    $vehicles = glob('./img/vehicles/*');
+    switch($pic) {
+        case "landscapes":
+            return $landscapes;
+        case "animals":
+            return $animals;
+        case "vehicles":
+            return $vehicles;
+        case "all":
+            return array_merge($landscapes, $animals, $vehicles);
+        default:
+//            throw new Exception("Invalid mode value");
+    }
+}
+
+$list = getList($_REQUEST["mode"]);
+
+
+
+//$allPhotos = array_merge($landscapes, $animals, $vehicles);
 //var_dump($landscapes);
 //var_dump($animals);
 //var_dump($vehicles);
-var_dump($allPhotos);
+//var_dump($allPhotos);
+var_dump($list);
 
 
 //die('{"items": [{ "id": 1, "name": "Apples",  "price": "$2" }, { "id": 2, "name": "Peaches", "price": "$5" }]}');
