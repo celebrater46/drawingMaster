d03231b570cabc880152074eeefa3b15aa45e961
hideru <jota.16.stigma.world@gmail.com>
2021-07-06 17:53:29 +0900

    moved ajax and removed ajax.js

--- main.js
+++ main.js
@@ -3,16 +3,7 @@
 // リピート再生とランダム再生
 
 {
-    const images = [
-        "img/pic00.png",
-        "img/pic01.png",
-        "img/pic02.png",
-        "img/pic03.png",
-        "img/pic04.png",
-        "img/pic05.png",
-        "img/pic06.png",
-        "img/pic07.png",
-    ];
+    let images = [];
     let currentIndex = 0;
     const mainImage = document.getElementById("mainImg");
     mainImage.src = images[currentIndex]; // Not "textContent"
@@ -39,37 +30,75 @@
         document.querySelector(".thumbnails").appendChild(li);
     });
 
-    const nextButton = document.getElementById("next");
-    nextButton.addEventListener("click", () => {
+    // const nextButton = document.getElementById("next");
+    // nextButton.addEventListener("click", () => {
+    //     let target = currentIndex + 1;
+    //     if (target === images.length) {
+    //         target = 0;
+    //     }
+    //     document.querySelectorAll(".thumbnails > li")[target].click();
+    // });
+
+    const changePicture = () => {
         let target = currentIndex + 1;
         if (target === images.length) {
             target = 0;
         }
         document.querySelectorAll(".thumbnails > li")[target].click();
     });
-
-    const prevButton = document.getElementById("prev");
-    prevButton.addEventListener("click", () => {
-        let target = currentIndex - 1;
-        if (target < 0) {
-            target = images.length - 1;
-        }
-        document.querySelectorAll(".thumbnails > li")[target].click();
-    });
+    // const prevButton = document.getElementById("prev");
+    // prevButton.addEventListener("click", () => {
+    //     let target = currentIndex - 1;
+    //     if (target < 0) {
+    //         target = images.length - 1;
+    //     }
+    //     document.querySelectorAll(".thumbnails > li")[target].click();
+    // });
 
     let isPlaying = false;
     let timeoutId;
 
     function playSlideshow() {
         timeoutId = setTimeout(() => {
-            nextButton.click();
+            // nextButton.click();
+            changePicture();
             playSlideshow();
         }, 1000);
     }
 
+    // Get the list of image files from main.php
+    $('#btnsend').on('click', function(){
+        $('#result').text('通信中...');
+        // Ajax通信を開始
+        $.ajax({
+            url: 'main.php',
+            type: 'GET',
+            dataType: 'text',
+            // フォーム要素の内容をハッシュ形式に変換
+            data: $('form').serializeArray(),
+            timeout: 5000,
+        }).done(function(data) {
+            // 通信成功時の処理を記述
+            console.log("Succeeded: data is ...");
+            // console.log(data);
+            images = data;
+            $("#result").text(data);
+        }).fail(function(jqXHR, textStatus) {
+            // 通信失敗時の処理を記述
+            console.log("Failed:");
+            // console.log(textStatus);
+            $("#result").text(textStatus);
+        }).always(function() {
+            console.log( "ajax complete" );
+        });
+    })
+
+    // When pushed the play button
     const play = document.getElementById("play");
+    const btnSend = document.getElementById("btnsend");
     play.addEventListener("click", () => {
         if (isPlaying === false) {
+            btnSend.click();
             playSlideshow();
             play.textContent = "Pause";
         } else {
